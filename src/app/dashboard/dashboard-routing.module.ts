e18050df5c3b42e97accc6d14fa75040c51cd5c8
salahel-sadany <salahelsadany@outlook.com>
2024-11-28 20:26:25 +0200

    header finished

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -4,14 +4,15 @@ import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent,
-    children:[
-      {path:'home',component:HomeComponent}
-    ]
-   }];
+  {
+    path: '',
+    component: DashboardComponent,
+    children: [{ path: 'home', component: HomeComponent }],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class DashboardRoutingModule { }
+export class DashboardRoutingModule {}
