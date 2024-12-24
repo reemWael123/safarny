7b782fde8c9fb114cb306d2cd61245852abe93d0
salahel-sadany <salahelsadany@outlook.com>
2024-12-24 22:10:08 +0200

    categories pages

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { HomeComponent } from './components/home/home.component';
+import { EgyptPageComponent } from './components/categories-pages/egypt-page/egypt-page.component';
+import { BeachesPageComponent } from './components/categories-pages/beaches-page/beaches-page.component';
+import { CitiesPageComponent } from './components/categories-pages/cities-page/cities-page.component';
+import { SportsPageComponent } from './components/categories-pages/sports-page/sports-page.component';
 
 const routes: Routes = [
   {
     path: '',
     component: DashboardComponent,
-    children: [{ path: 'home', component: HomeComponent }],
+    children: [
+      {
+        path: 'home',
+        component: HomeComponent,
+        // children: [{ path: 'ancient', component: EgyptPageComponent }],
+      },
+      { path: 'home/ancient-egypt', component: EgyptPageComponent },
+      { path: 'home/beaches', component: BeachesPageComponent },
+      { path: 'home/cities', component: CitiesPageComponent },
+      { path: 'home/outdoor-sports', component: SportsPageComponent },
+    ],
   },
 ];
 
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -7,19 +7,31 @@ import { DashboardComponent } from './dashboard.component';
 import { SearchHomeComponent } from './components/search-home/search-home.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeroComponent } from './components/hero/hero.component';
-
+import { EgyptPageComponent } from './components/categories-pages/egypt-page/egypt-page.component';
+import { BeachesPageComponent } from './components/categories-pages/beaches-page/beaches-page.component';
+import { CategPageComponent } from './components/categories-pages/categ-page/categ-page.component';
+import { CitiesPageComponent } from './components/categories-pages/cities-page/cities-page.component';
+import { SportsPageComponent } from './components/categories-pages/sports-page/sports-page.component';
+import { RouterLink, RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     SearchHomeComponent,
     HomeComponent,
-    HeroComponent
+    HeroComponent,
+    EgyptPageComponent,
+    BeachesPageComponent,
+    CategPageComponent,
+    CitiesPageComponent,
+    SportsPageComponent,
   ],
   imports: [
     CommonModule,
+    RouterLink,
+    RouterModule,
     DashboardRoutingModule,
-    SharedModule
-  ]
+    SharedModule,
+  ],
 })
-export class DashboardModule { }
+export class DashboardModule {}
